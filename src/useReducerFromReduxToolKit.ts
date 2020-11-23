ad2fe3e66357fc3f246fc0a1e7b1d06ad99a1286
Luis Puig <luis@revieve.com>
2020-11-23 14:31:47 +0100

    Add types

--- src/useReducerFromReduxToolKit.ts
+++ src/useReducerFromReduxToolKit.ts
@@ -1,7 +1,13 @@
 import { useReducer, useMemo } from "react";
-import { Slice } from "@reduxjs/toolkit";
+import { Slice, SliceCaseReducers, CaseReducerActions } from "@reduxjs/toolkit";
 
-export const useReducerFromReduxToolKit = <T extends Slice>(slice: T) => {
+export const useReducerFromReduxToolKit = <
+  State,
+  CaseReducers extends SliceCaseReducers<State>,
+  Name extends string
+>(
+  slice: Slice<State, CaseReducers, Name>
+): [State, CaseReducerActions<CaseReducers>] => {
   const [state, dispatch] = useReducer(
     slice.reducer,
     slice.reducer(undefined, { type: "INIT" })
@@ -13,10 +19,9 @@ export const useReducerFromReduxToolKit = <T extends Slice>(slice: T) => {
           ...acc,
           [actionName]: (payload: any) => dispatch(action(payload)),
         }),
-        {}
+        {} as CaseReducerActions<CaseReducers>
       ),
     [slice.actions]
   );
-
   return [state, actions];
 };
